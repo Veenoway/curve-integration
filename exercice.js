5dc988e723ec5df981d39f203fa5a2148458b98a
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 00:44:50 +0100

    feature: initialise front end side with gsap

--- exercice.js
+++ exercice.js
@@ -20,7 +20,30 @@ require("dotenv").config();
 const provider = new ethers_1.ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 const swapEventListener = () => __awaiter(void 0, void 0, void 0, function* () {
     const blockNumber = yield provider.getBlockNumber();
-    const contract = new ethers_1.ethers.Contract(abi_1.PAIR_ADDRESS, abi_1.UNISWAP_ABI, provider);
+    const contract = new ethers_1.ethers.Contract(abi_1.CURVE_FACTORY_ADDRESS, abi_1.CURVE_ABI, provider);
+    contract.on("Swap", (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
+        const events = {
+            sender,
+            amount0In,
+            amount1In,
+            amount0Out,
+            amount1Out,
+            to,
+        };
+        const swapEvent = JSON.stringify(events);
+        const inAmount = (0, utils_1.getAmount)(amount0In, amount1In);
+        const outAmount = (0, utils_1.getAmount)(amount0Out, amount1Out);
+        fs_1.default.appendFile("swap.json", swapEvent + ",", (err) => {
+            if (err)
+                throw err;
+            if (utils_1.routerName[sender])
+                (0, utils_1.getLog)("Swap has been recorded from: ", inAmount, outAmount, sender);
+            else if (utils_1.routerName[to])
+                (0, utils_1.getLog)("Swap has been recorded to: ", inAmount, outAmount, to);
+            else
+                (0, utils_1.getLog)("Swap event saved! ", inAmount, outAmount);
+        });
+    });
     contract.on("Swap", (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
         const events = {
             sender,
--- exercice.ts
+++ exercice.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import fs from "fs";
-import { PAIR_ADDRESS, UNISWAP_ABI } from "./abi";
+import { CURVE_ABI, CURVE_FACTORY_ADDRESS } from "./abi";
 import { SwapEventProps } from "./interface";
 import { getAmount, getLog, routerName } from "./utils";
 require("dotenv").config();
@@ -10,7 +10,37 @@ const provider = new ethers.providers.JsonRpcProvider(
 );
 const swapEventListener = async () => {
   const blockNumber = await provider.getBlockNumber();
-  const contract = new ethers.Contract(PAIR_ADDRESS, UNISWAP_ABI, provider);
+  const contract = new ethers.Contract(
+    CURVE_FACTORY_ADDRESS,
+    CURVE_ABI as any,
+    provider
+  );
+
+  contract.on(
+    "Swap",
+    (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
+      const events: SwapEventProps = {
+        sender,
+        amount0In,
+        amount1In,
+        amount0Out,
+        amount1Out,
+        to,
+      };
+      const swapEvent = JSON.stringify(events);
+      const inAmount = getAmount(amount0In, amount1In);
+      const outAmount = getAmount(amount0Out, amount1Out);
+
+      fs.appendFile("swap.json", swapEvent + ",", (err) => {
+        if (err) throw err;
+        if (routerName[sender])
+          getLog("Swap has been recorded from: ", inAmount, outAmount, sender);
+        else if (routerName[to])
+          getLog("Swap has been recorded to: ", inAmount, outAmount, to);
+        else getLog("Swap event saved! ", inAmount, outAmount);
+      });
+    }
+  );
 
   contract.on(
     "Swap",
