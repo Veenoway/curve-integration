f9543e1622483440571f9a0d605dedb85cad9d5e
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 01:29:20 +0100

    feature: start end on scrollTrigger

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ gsap.to(".square", {
   x: 700,
   duration: 3,
   ease: "bounce",
+  scrollTrigger: {
+    trigger: ".square",
+    start: "top center",
+    end: "bottom center",
+    markers: true,
+    toggleClass: "red",
+  },
 });
