35b0cbd9e96b2c9666746e494f3746929ca40695
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 09:52:16 +0100

    feature: gsap pin option

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 gsap.to(".square", {
   x: 1000,
-  duration: 3,
+  duration: 8,
   scrollTrigger: {
-    trigger: ".square",
+    trigger: ".square2",
     start: "top 80%",
     end: "top 30%",
     markers: true,
     toggleClass: "red",
     scrub: true,
+    pin: ".square",
+    pinSpacing: true,
     toggleActions: "restart none none none",
     //          onEnter, onLeave, onEnterBack, onLeaveBack
     // options: play, pause, resume, reset, restart, complete, reverse, none
