471fc91461bd1c8da89e0e125689f506c1a0b001
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 13:29:06 +0100

    feature: animation text

--- app.js
+++ app.js
@@ -33,19 +33,27 @@ tl.to(".logo", {
   y: 500,
   scale: 1.6,
   rotate: 240,
-  duration: 10,
+  duration: 6,
 });
 
 tl.to(
   ".logo",
   {
-    y: 850,
+    y: () => {
+      const secondBox = document.querySelector(".second-box");
+      const logo = document.querySelector(".logo");
+
+      const secondBoxCenterY = secondBox.offsetTop + secondBox.offsetHeight / 2;
+      const logoCenterY = logo.offsetTop + logo.offsetHeight / 2;
+
+      return secondBoxCenterY - logoCenterY;
+    },
     x: 350,
-    scale: 1.9,
+    scale: "+=0.3",
     rotate: 360,
     duration: 6,
   },
-  "+=0"
+  ">=0"
 );
 
 gsap.to(".title", {
@@ -58,10 +66,48 @@ gsap.to(".title", {
     start: "bottom 100%",
     end: "bottom 80%",
     scrub: true,
-    pin: ".title",
-    pinSpacing: false,
     toggleActions: "restart none none none",
     //          onEnter, onLeave, onEnterBack, onLeaveBack
     // options: play, pause, resume, reset, restart, complete, reverse, none
   },
 });
+
+gsap.fromTo(
+  ".subtitle",
+  {
+    x: -200,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 4,
+    scrollTrigger: {
+      trigger: ".second-box",
+      start: "top center",
+      end: "center center",
+      scrub: true,
+      toggleActions: "restart none none none",
+      //          onEnter, onLeave, onEnterBack, onLeaveBack
+      // options: play, pause, resume, reset, restart, complete, reverse, none
+    },
+  }
+);
+
+gsap.fromTo(
+  ".description",
+  { x: -100, opacity: 0 },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 4,
+    delay: 4,
+    scrollTrigger: {
+      trigger: ".second-box",
+      start: "top+=100 center",
+      end: "center+=100 center",
+      scrub: true,
+      toggleActions: "restart none none none",
+    },
+  }
+);
