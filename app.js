57861dd99048070ef73221fb4e216ab0c828dac8
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 09:39:33 +0100

    feature: learning toggleAction method

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 gsap.to(".square", {
-  x: 700,
+  x: 1000,
   duration: 3,
   ease: "bounce",
   scrollTrigger: {
     trigger: ".square",
-    start: "top center",
-    end: "bottom center",
+    start: "top 30%",
+    end: "bottom 30%",
     markers: true,
     toggleClass: "red",
+    toggleActions: "restart reverse restart reverse",
+    //          onEnter, onLeave, onEnterBack, onLeaveBack
+    // options: play, pause, resume, reset, restart, complete, reverse, none
   },
 });
