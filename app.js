53d0001f1a393432bde31b9e6da16ce25a9f950f
Veeno <sebastien.cloiseau@gmail.com>
2023-12-25 09:46:46 +0100

    feature: adding scrub

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 gsap.to(".square", {
   x: 1000,
   duration: 3,
-  ease: "bounce",
   scrollTrigger: {
     trigger: ".square",
-    start: "top 30%",
-    end: "bottom 30%",
+    start: "top 80%",
+    end: "top 30%",
     markers: true,
     toggleClass: "red",
-    toggleActions: "restart reverse restart reverse",
+    scrub: true,
+    toggleActions: "restart none none none",
     //          onEnter, onLeave, onEnterBack, onLeaveBack
     // options: play, pause, resume, reset, restart, complete, reverse, none
   },
