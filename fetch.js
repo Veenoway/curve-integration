871c41d4e81ead39b6e7e09e4f403c1b04e1b3a2
Veeno <sebastien.cloiseau@gmail.com>
2023-12-26 06:47:37 +0100

    feature: uncode hex value

--- fetch.js
+++ fetch.js
@@ -1,15 +1,16 @@
 fetch("./events_curve.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     const lastElement = data[data.length - 1];
     const keys = Object.keys(lastElement);
 
     keys.forEach((key) => {
       const value = lastElement[key];
       const element = document.getElementById(key);
-      if (value?.hex) element.innerHTML = `${key}: ${value.hex}`;
-      else element.innerHTML = `${key}: ${value}`;
+      if (value?.hex) {
+        const formattedValue = parseInt(value.hex, 16);
+        element.innerHTML = `${key}: ${formattedValue}`;
+      } else element.innerHTML = `${key}: ${value}`;
     });
   })
   .catch((error) =>
