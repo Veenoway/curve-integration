40a30c7b1ca9944346e791fa6c2f9ebd32e1d206
Veeno <sebastien.cloiseau@gmail.com>
2023-12-26 07:20:44 +0100

    feature: adding remove liquidity on fed

--- fetch.js
+++ fetch.js
@@ -16,3 +16,22 @@ fetch("./events_curve.json")
   .catch((error) =>
     console.error("Erreur lors du chargement du fichier JSON:", error)
   );
+
+fetch("./remove_liquidity_one.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const lastElement = data[data.length - 1];
+    const keys = Object.keys(lastElement);
+
+    keys.forEach((key) => {
+      const value = lastElement[key];
+      const element = document.getElementById(key);
+      if (value?.hex) {
+        const formattedValue = parseInt(value.hex, 16);
+        element.innerHTML = `${key}: ${formattedValue}`;
+      } else element.innerHTML = `${key}: ${value}`;
+    });
+  })
+  .catch((error) =>
+    console.error("Erreur lors du chargement du fichier JSON:", error)
+  );
